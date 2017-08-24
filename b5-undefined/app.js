24ebd0618a36263058d89f1021a9f8254049d4b2
brettc-84 <knupknilb@gmail.com>
2017-08-24 17:10:09 +0200

    b5-undefined

---
+++ b5-undefined/app.js
@@ -0,0 +1,16 @@
+var a;
+console.log(a);
+
+if (a===undefined) {
+    console.log('a is undefined');
+} else {
+    console.log('a is defined');
+}
+
+a = 'aaa';
+
+if (a===undefined) {
+    console.log('a is undefined');
+} else {
+    console.log('a is defined');
+}
