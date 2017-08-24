fe1ff4b7ce55c96e0c1bbcb0910ad8d97e41241a
brettc-84 <knupknilb@gmail.com>
2017-08-24 17:35:38 +0200

    lecture 15: variable environments + main index.html

---
+++ b9-variable_environments/app.js
@@ -0,0 +1,18 @@
+// Each global execution context has it's own variable space
+// *scope*
+
+function b() {
+    var myVar;
+    console.log(myVar);
+}
+
+function a() {
+    var myVar = 2;
+    console.log(myVar);
+    b();
+}
+
+var myVar = 1;
+console.log(myVar);
+a();
+console.log(myVar);
